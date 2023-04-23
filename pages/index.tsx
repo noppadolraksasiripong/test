53e2c4c817f11b57395b2fe6564d08e968e75b63
noppadolraksasiripong <noppadol.raksasiripong@sagecapital.com.sg>
2023-04-24 01:30:10 +0700

    implement header and footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,72 +1,126 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 import ComponentToPrint from '@/components/ComponentToPrint'
 import jsPDF from 'jspdf'
-// import { applyPlugin } from 'jspdf-autotable'
 import React, { useRef } from 'react'
 import { toPng } from 'html-to-image'
 export default function Home() {
   // const button = document.getElementById("download-button");
   const refNode = useRef<HTMLDivElement>(null)
   async function generatePDF() {
-
-    function getChildrenHeight(children: Element) {
-      const height = children.scrollHeight
-      return ['border-top-width', 'border-bottom-width', 'margin-top', 'margin-bottom']
-        // get array of margin and border
-        .map(side => parseFloat(window.getComputedStyle(children)
-          .getPropertyValue(side)
-          .replace('px', '')
-        ))
-        //sum it with height
-        .reduce((total, side) => total + side, height)
-    }
-
     if (refNode.current) {
-
       //default state
       const ratio = 1.4145 // A4 ratio (width:height)
-      // const ratioWidth = 0.821 // canvas to pdf
       const width = 1240 // Set a default width for the PDF (in pixels)
-      const height = Math.trunc(width * ratio) // relate to width
-      const tableClassName = 'pdf-table'
+      const height = Math.trunc(width * ratio)
+      let page = 1
+      let footerHeight = 0
+      let headerHeight = 0
       let usedHeight = 0
-      //create PDF
       const pdf = new jsPDF("p", "pt", [width, height], true)
-      //more method when use table
-      if (refNode.current.className.search(tableClassName) === -1) {
-        console.log('use table')
 
+      const header = document.getElementById('header-pdf')
+      let headerDataURL = ''
+      let footerDataURL = ''
+      if (header) {
+        headerHeight = getElementHeight(header)
+        headerDataURL = await toPng(header as HTMLElement)
+        addHeader()
+      }
+
+      const footer = document.getElementById('footer-pdf')
+      if (footer) {
+        footerHeight = getElementHeight(footer)
+        footerDataURL = await toPng(footer as HTMLElement)
+        addFooter()
       }
 
       //loop for each children div
       for (let i = 0; i < refNode.current.children.length; i++) {
         const children = refNode.current.children[i]
-        const childrenHeight = getChildrenHeight(children)
+        const childrenHeight = getElementHeight(children)
         const canvasDataURL = await toPng(children as HTMLElement)
-
-        const canInsert = childrenHeight + usedHeight > height
-        if (canInsert) {
-          pdf.addPage() //8.5" x 11" in pts (in*72)
+        const isFull = childrenHeight + usedHeight > height
+        if (isFull) {
+          if (page === 1) {
+            addFooterPageNumber()
+          }
+          addPage()
           //reset useHeight
-          usedHeight = 0
-          pdf.addImage(canvasDataURL, 'PNG', 0, usedHeight, (width), childrenHeight)
-          usedHeight += childrenHeight
-        } else {
-          pdf.addImage(canvasDataURL, 'PNG', 0, usedHeight, (width), childrenHeight)
-          usedHeight += childrenHeight
+          usedHeight = 0 + footerHeight + headerHeight
+        }
+        addImage(canvasDataURL, usedHeight - footerHeight, childrenHeight)
+      }
+      if (pdf.getCurrentPageInfo().pageNumber === 1) {
+        addFooterPageNumber()
+      }
+
+      pdf.save('New.pdf')
+
+
+      function addFooterPageNumber() {
+        const text = `page: ${page}`
+        var footerX = pdf.internal.pageSize.getWidth() / 2
+        var footerY = pdf.internal.pageSize.getHeight() - 10
+        pdf.text(text, footerX, footerY, { align: 'center' })
+      }
+
+      function addImage(
+        canvasDataURL: string | HTMLImageElement,
+        startedHeight: number,
+        childrenHeight: number,
+      ) {
+        pdf.addImage(
+          canvasDataURL,
+          'PNG',
+          0,
+          startedHeight,
+          width,
+          childrenHeight
+        )
+        usedHeight += childrenHeight
+      }
+
+      function addPage() {
+        pdf.addPage() //8.5" x 11" in pts (in*72)
+        page++
+        addFooterPageNumber() //`page: ${page}`
+        if (header) {
+          addHeader()
+        }
+        if (footer) {
+          addFooter()
         }
       }
-      pdf.save('New.pdf')
+
+      function addFooter() {
+        addImage(footerDataURL, height - footerHeight, footerHeight)
+      }
+
+      function addHeader() {
+        addImage(headerDataURL, 0, headerHeight)
+      }
     }
   }
 
-
-
-
   return (
     <>
+      <header id="header-pdf">This is header</header>
       <button onClick={generatePDF}>Print this out!</button>
       <ComponentToPrint ref={refNode} />
+      <footer id="footer-pdf">This is footer</footer>
     </>
   )
+}
+
+
+function getElementHeight(children: Element) {
+  const height = children.scrollHeight
+  return ['border-top-width', 'border-bottom-width', 'margin-top', 'margin-bottom']
+    // get array of margin and border
+    .map(side => parseFloat(window.getComputedStyle(children)
+      .getPropertyValue(side)
+      .replace('px', '')
+    ))
+    //sum it with height
+    .reduce((total, side) => total + side, height)
 }
